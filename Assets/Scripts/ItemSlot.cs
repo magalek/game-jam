2da375e404ebb279625067e981c775d1d50fab0c
magalek <filipwalczak98@gmail.com>
2020-07-10 23:09:34 +0200

    Added serializable attribute

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class ItemSlot : MonoBehaviour
 {
     [SerializeField] private Item item;
