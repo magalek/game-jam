10cae3910557a901aeedbf0fc786ede8cbb7bffc
magalek <filipwalczak98@gmail.com>
2020-07-11 01:25:49 +0200

    Added second scene and some other shit

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI resultTextObject;
+    [SerializeField] private TextMeshProUGUI nameTextObject;
+
+    private Event @event;
+
+    private void Start() {
+        @event = Resources.LoadAll<Event>("Events")[0];
+        
+        nameTextObject.text = @event.name;
+        resultTextObject.text = @event.description;
+    }
+
     private void Update() {
         if (Input.GetKeyDown(KeyCode.A)) {
-            Event @event = Resources.LoadAll<Event>("Events")[0];
+            
+            bool success = Random.Range(0, 2) == 1 ? true : false;
 
-            int random = Random.Range(0, @event.Items.Count);
-
-            string message = string.Format(@event.description, @event.Items[random].item.firstSuccesMessage, @event.Items[random].item.failMessage);
-
-            Debug.Log(message);
+            UpdateText(success, @event);
         }
+    }
+
+    private void UpdateText(bool success, Event @event) {
+        int random = Random.Range(0, @event.Items.Count);
+        if (success)
+            resultTextObject.text = string.Format(@event.succesMessage, @event.Items[random].option == Option.First ? @event.Items[random].item.firstSuccesMessage : @event.Items[random].item.secondSuccesMessage);
+        else
+            resultTextObject.text = string.Format(@event.failMessage, @event.Items[random].item.failMessage);
     }
 }
