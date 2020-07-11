ded131930fb8483b14105d18332e2fc0861fc1c2
magalek <filipwalczak98@gmail.com>
2020-07-12 00:28:44 +0200

    Added title text on event message

---
+++ Assets/EventMessage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EventMessage : MonoBehaviour
+{
+    public TextMeshProUGUI description;
+    public TextMeshProUGUI title;
+}
